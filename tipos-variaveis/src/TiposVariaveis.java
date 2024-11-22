7402a543b873f7909173434443ebdabb30085200
Thiago <thiago.pires.cs@gmail.com>
2024-11-22 16:29:56 -0300

    Commit para atualização de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World");
+
+        int numero = 1;
+
+        numero = 3;
+        
+        System.out.println(numero);
+
+        final double VALOR_PI = 3.14;
+
+       
+
+        System.err.println(VALOR_PI);
+
     }
 }
